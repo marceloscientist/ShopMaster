329b0ffb2a8df94fdfd234549dd484efef0f77fc
MARCELO DE CARVALHO SANTANA <marcelo.c.santana+bradesco@bradesco.com.br>
2026-05-09 04:17:34 -0300

    Initial Commit

---
+++ ShopMaster/ShopMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopMasterApp.swift
+//  ShopMaster
+//
+//  Created by Santana, Marcelo de Carvalho on 09/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ShopMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
